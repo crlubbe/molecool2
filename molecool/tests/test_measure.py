3588f52f62bb03e98f081df9ad80d205612eec84
Caroline Lubbe <clubbe@nd.edu>
2021-06-01 14:11:19 -0500

    added another test for molecular mass

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -6,6 +6,7 @@ Tests for the measure module.
 from molecool.measure import calculate_angle, calculate_distance
 import molecool
 import numpy as np
+import pytest
 
 def test_calculate_distance():
 
@@ -21,12 +22,21 @@ def test_calculate_distance():
 
 def test_calculate_angle():
 
-    r1 = np.array([1,0,0])
+    r1 = np.array([0,0,-1])
     r2 = np.array([0,0,0])
-    r3 = np.array([-1,0,0])
+    r3 = np.array([1,0,0])
 
-    expected_angle = 180
+    expected_angle = 90
 
     calculated_angle = molecool.calculate_angle(r1,r2,r3,degrees=True)
 
-    assert expected_angle == calculated_angle
+    assert pytest.approx(expected_angle) == calculated_angle
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle",
+    [(np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0,0,0]), np.array([1,0,0]), 45),
+    (np.array([0,0,-1]), np.array([0,1,0]), np.array([1,0,0]), 60)]
+)
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+    calculated_value = molecool.calculate_angle(p1,p2,p3,degrees=True)
+
+    assert pytest.approx(expected_angle) == calculated_value
