f1c0ed4259c2f013d8187b5b4d9bf1e21ba00336
Caroline Lubbe <clubbe@nd.edu>
2021-06-01 11:56:30 -0500

    added test files

---
+++ molecool/tests/test_measure.py
@@ -0,0 +1,32 @@
+"""
+Tests for the measure module.
+"""
+
+# imports
+from molecool.measure import calculate_angle, calculate_distance
+import molecool
+import numpy as np
+
+def test_calculate_distance():
+
+    r1 = np.array([0,0,0])
+    r2 = np.array([0,1,0])
+
+    expected_distance = 1
+
+    calculated_distance = molecool.calculate_distance(r1,r2)
+
+    assert expected_distance == calculated_distance
+
+
+def test_calculate_angle():
+
+    r1 = np.array([1,0,0])
+    r2 = np.array([0,0,0])
+    r3 = np.array([-1,0,0])
+
+    expected_angle = 180
+
+    calculated_angle = molecool.calculate_angle(r1,r2,r3,degrees=True)
+
+    assert expected_angle == calculated_angle
